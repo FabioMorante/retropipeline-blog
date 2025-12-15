99a463c27706b27b4e3541a5d4279c27c759c1af
Fabio Morante <fabiomorante@outlook.com>
2025-12-14 23:24:10 -0500

    FEAT: Se hacen ajustes básicos al template

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'RetroPipeline.dev';
+export const SITE_DESCRIPTION = 'Data, desarrollo, JRPG retro, anime y cine previo a los 2020s';
